78961a6e6f2b55e295fec815e2b40775f6796298
lex <alex@bighill.org>
2019-10-18 20:43:29 -0700

    update reamde

--- js/init.js
+++ js/init.js
@@ -8,14 +8,8 @@
 
 var init = function()
 {
-    console.group( 'init()' );
-
     Toggle.listeners();
-    console.log( 'add listeners' );
-
     document.removeEventListener( "DOMContentLoaded", init );
-
-    console.groupEnd();
 };
 document.addEventListener( 'DOMContentLoaded', init );
 
